5487e894799b4dfce4401d66a3b759b5cff335d0
Jayneet7 <58805705+Jayneet7@users.noreply.github.com>
2020-09-29 12:24:06 +0300

    1st copy
    Prototype

---
+++ Assets/Game scripts/Centercolition.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Centercolition : MonoBehaviour
+{
+    
+    public GameObject enterTextA;
+    public GameObject enterTextB;
+    public GameObject enterTextC;
+    public GameObject enterTextD;
+    
+
+    void Start()
+    {
+        enterTextA.SetActive(false);
+        enterTextB.SetActive(false);
+        enterTextC.SetActive(false);
+        enterTextD.SetActive(false);
+    
+
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D carda)
+    {
+        
+        if(carda.gameObject.tag == "Card A")
+        {
+            enterTextA.SetActive(true);
+        }
+        if (carda.gameObject.tag == "Card B")
+        {
+            enterTextB.SetActive(true);
+        }
+        if (carda.gameObject.tag == "Card C")
+        {
+            enterTextC.SetActive(true);
+        }
+        if (carda.gameObject.tag == "Card D")
+        {
+            enterTextD.SetActive(true);
+        }
+        
+    }
+
+
+
+}
---
+++ Assets/Game scripts/Movecharecter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movecharecter : MonoBehaviour
+{
+    public float speed = 04.0f;
+    private void Movement()
+    {
+        Vector3 pos = transform.position;
+        if (Input.GetKey("w"))
+        {
+            pos.y += speed * Time.deltaTime;
+        }
+        if (Input.GetKey("s"))
+        {
+            pos.y -= speed * Time.deltaTime;
+        }
+        if (Input.GetKey("a"))
+        {
+            pos.x -= speed * Time.deltaTime;
+        }
+        if (Input.GetKey("d"))
+        {
+            pos.x += speed * Time.deltaTime;
+        }
+
+        transform.position = pos;
+    }
+
+    public void Update()
+    {
+        Movement();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+      //  Debug.Log("Trigger");
+    }
+}
